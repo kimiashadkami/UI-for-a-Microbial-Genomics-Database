1828fd9a4633f32d6c4e1cd0b172681c1f533ec9
KimiaShadkami <shadkamk@myumanitoba.ca>
2021-07-29 04:36:03 -0400

    using svg in a responsive manner to visualize some dummy data

--- static/js/vis.js
+++ static/js/vis.js
@@ -1,37 +1,64 @@
 console.log(d3);
 
 const DUMMY_DATA = [
-  {id: 'd1', value: 10, name: 'A'},
-  {id: 'd2', value: 30, name: 'B'},
-  {id: 'd3', value: 5, name: 'C'},
-  {id: 'd4', value: 20, name: 'D'},
-  {id: 'd5', value: 15, name: 'E'},
-  {id: 'd6', value: 25, name: 'F'},
-  {id: 'd7', value: 35, name: 'G'},
-  {id: 'd8', value: 5, name: 'H'},
-  {id: 'd9', value: 20, name: 'I'},
-  {id: 'd10', value: 15, name: 'J'},
-  {id: 'd11', value: 25, name: 'K'},
-  {id: 'd12', value: 35, name: 'L'}
+  {id: 'd1', name: 'A', value: 10},
+  {id: 'd2', name: 'B', value: 30},
+  {id: 'd3', name: 'C', value: 5},
+  {id: 'd4', name: 'D', value: 20},
+  {id: 'd5', name: 'E', value: 15},
+  {id: 'd6', name: 'F', value: 7},
+  {id: 'd7', name: 'G', value: 11},
+  {id: 'd8', name: 'H', value: 33},
+  {id: 'd9', name: 'I', value: 32},
+  {id: 'd10', name: 'J', value: 32},
+  {id: 'd11', name: 'K', value: 9},
+  {id: 'd12', name: 'L', value: 14}
 ]
 
+const width = 500;
+const height = 500;
+const margin = {top: 100, bottom: 100, left: 100, right: 100}
+
+const svg = d3.select('#d3-container')
+              .append('svg')
+              .attr('width', width - margin.left - margin.right)
+              .attr('heigh', height - margin.top - margin.bottom)
+              .attr('viewBox', [0, 0, width, height]);
+
 const x_scale = d3.scaleBand()
-                  .domain(DUMMY_DATA.map((dataPoint) => dataPoint.name))
-                  .rangeRound([0,250])
+                  .domain(d3.range(DUMMY_DATA.length))
+                  .range([margin.left, width - margin.right])
                   .padding(0.1);
-const y_scale = d3.scaleLinear().domain([0,40]).range([200,0]);
 
-const bars = d3.select('.test')
-               .selectAll('.bar')
-               .data(DUMMY_DATA)
-               .enter()
-               .append('rect')
-               .classed('bar', true)
-               .attr('width', x_scale.bandwidth())
-               .attr('height', data_point => 200 - y_scale(data_point.value))
-               .attr('x', data => x_scale(data.name))
-               .attr('y', data_point => y_scale(data_point.value));
+const y_scale = d3.scaleLinear()
+                  .domain([0,40])
+                  .range([height - margin.bottom, margin.top]);
 
-setTimeout(() => {
-  bars.data(DUMMY_DATA.slice(0,2).exit().remove())
-  }, 2000)
+//creating the bar chart
+svg
+  .append('g')
+  .attr('fill', 'royalblue')
+  .selectAll('rect')
+  .data(DUMMY_DATA)
+  .join('rect')
+    .attr('x', (d, i) => x_scale(i))
+    .attr('y', (d) => y_scale(d.value))
+    .attr('width', x_scale.bandwidth())
+    .attr('height', d => y_scale(0) - y_scale(d.value));
+
+//the x axis labels
+function xAxis(g){
+  g.attr('transform', `translate(0, ${height - margin.bottom})`)
+  .call(d3.axisBottom(x_scale).tickFormat(i => DUMMY_DATA[i].name))
+  .attr('font-size', '20px')
+}
+
+function yAxis(g){
+  g.attr('transform', `translate(${margin.left}, 0)`)
+  .call(d3.axisLeft(y_scale).ticks(null, DUMMY_DATA.format))
+  .attr('font-size', '20px')
+}
+
+svg.append('g').call(yAxis);
+svg.append('g').call(xAxis);
+svg.node();
