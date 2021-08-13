d8fd5312d86cdd00d374f92301d2e0d84c9eb81b
KimiaShadkami <shadkamk@myumanitoba.ca>
2021-08-12 20:23:05 -0400

    visualizing actual data using d3.js

--- static/js/vis.js
+++ static/js/vis.js
@@ -3,23 +3,31 @@ console.log(d3);
 function visualize(data){
   console.log(data);
 
-  const vis_data = data;
+  const VIS_DATA = data;
   const width = 500;
-  const height = 1000;
+  const height = VIS_DATA.length*25;
+  const min_height = 1000;
   const margin = {top: 100, bottom: 100, left: 100, right: 100};
+
+
+  VIS_DATA.sort(
+    function(g1, g2) {
+      return (g2['value']) - (g1['value']);
+    }
+  );
 
   const svg = d3.select('#d3-container')
                 .append('svg')
                 .attr('width', width - margin.left - margin.right)
-                .attr('height', height - margin.top - margin.bottom)
+                .attr('min-height', height - margin.top - margin.bottom)
                 .attr('viewBox', [0, 0, width, height]);
 
   const x_scale = d3.scaleLinear()
-                    .domain([0,40])
+                    .domain([0, VIS_DATA[0]['value']])
                     .range([margin.left, width - margin.right]);
 
   const y_scale = d3.scaleBand()
-                    .domain(d3.range(vis_data.length))
+                    .domain(d3.range(VIS_DATA.length))
                     .range([height - margin.bottom, margin.top])
                     .padding(0.1);
 
@@ -28,7 +36,7 @@ function visualize(data){
     .append('g')
     .attr('fill', 'royalblue')
     .selectAll('rect')
-    .data(vis_data)
+    .data(VIS_DATA)
     .join('rect')
       .attr('x', margin.left)
       .attr('y', (d, i) => y_scale(i))
@@ -37,20 +45,20 @@ function visualize(data){
       .attr('class', 'rectangle')
       .attr('data-toggle', 'tooltip')
       .attr('data-placement','right')
-      .attr('title', (d, i) => vis_data[i].name + ": " + vis_data[i].value)
+      .attr('title', (d, i) => VIS_DATA[i].name + ": " + VIS_DATA[i].value)
 
 
   //the x axis labels
   function xAxis(g){
     g.attr('transform', `translate(0, ${height - margin.bottom})`)
-    .call(d3.axisBottom(x_scale).ticks(null, vis_data.format))
-    .attr('font-size', '20px');
+    .call(d3.axisBottom(x_scale).ticks(null, VIS_DATA.format))
+    .attr('font-size', '100%');
   }
 
   function yAxis(g){
     g.attr('transform', `translate(${margin.left}, 0)`)
-    .call(d3.axisLeft(y_scale).tickFormat(i => vis_data[i].name))
-    .attr('font-size', '20px');
+    .call(d3.axisLeft(y_scale).tickFormat(i => VIS_DATA[i].name))
+    .attr('font-size', '100%');
   }
 
   svg.append('g').call(yAxis);
